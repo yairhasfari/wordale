54d783c229db733cd516f43c32aa5f66e4c7cb60
yairhasfari <yairhasfari@gmail.com>
2025-04-17 16:40:59 +0300

    Update summer clock main.js

--- main.js
+++ main.js
@@ -36,7 +36,7 @@ let guessDistribution;
 function pickWord() {
     //today = new Date();
     var differenceInTime = today.getTime() - startDate.getTime();
-    //    var differenceInTime = today.getTime() - summerClockStartDate.getTime();
+     //  var differenceInTime = today.getTime() - summerClockStartDate.getTime();
 
     // To calculate the no. of days between two dates
     var differenceInDays = Math.floor(differenceInTime / (1000 * 3600 * 24)); //added 74 since it screwed the 1 hour difference between gmt+3 and gmt+2; 
